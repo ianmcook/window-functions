9d659040fe8880bd1c9016627bfdee47ae241c66
Ian Cook <ianmcook@gmail.com>
2021-11-30 18:20:40 -0500

    Add DuckDB create and load table instructions

--- create-table-daylight.sql
+++ create-table-daylight.sql
@@ -1,5 +1,7 @@
-CREATE TABLE daylight (
-		month TINYINT,
-		day TINYINT,
-		light DECIMAL(6,4))
-	ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+-- DuckDB
+CREATE TABLE daylight(month TINYINT, day TINYINT, light DECIMAL(6,4));
+COPY daylight from 'daylight.tsv' (AUTO_DETECT TRUE);
+
+-- Impala or Hive
+CREATE TABLE daylight (month TINYINT, day TINYINT, light DECIMAL(6,4)) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+-- then copy daylight.tsv into the table directory
