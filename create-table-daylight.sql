27cd3011a7ba9691f656c345d383c819957f1101
Ian Cook <ianmcook@gmail.com>
2021-12-02 13:30:42 -0500

    Delete create-table-daylight.sql

--- create-table-daylight.sql
+++
@@ -1,7 +0,0 @@
--- DuckDB
-CREATE TABLE daylight(month TINYINT, day TINYINT, light DECIMAL(6,4));
-COPY daylight from 'daylight.tsv' (AUTO_DETECT TRUE);
-
--- Impala or Hive
-CREATE TABLE daylight (month TINYINT, day TINYINT, light DECIMAL(6,4)) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
--- then copy daylight.tsv into the table directory
